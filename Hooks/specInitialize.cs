bf52ba5288423e811e3051d97efc44395c70fce9
ShraavyaKaranth <shravyabahha@gmail.com>
2025-03-19 15:21:25 +0530

    Update specInitialize.cs

--- Hooks/specInitialize.cs
+++ Hooks/specInitialize.cs
@@ -1,26 +1,52 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AppiumSpecFlowProject1.Utilities;
 using NUnit.Framework;
+using TechTalk.SpecFlow;
 
 namespace AppiumSpecFlowProject1.Hooks
 {
     [Binding]
-    public partial class specInitialize:Base
+    public partial class SpecInitialize : Base
     {
         [BeforeScenario]
         public void TestInitializeTest()
         {
-            AndroidContext = StartAppiumServerForHybrid();
-            ScenarioContext.Current["androidContext"] = AndroidContext;
+            try
+            {
+                Console.WriteLine("Initializing Appium for Scenario...");
+                AndroidContext = StartAppiumServerForHybrid();
+
+                if (ScenarioContext.Current.ContainsKey("androidContext"))
+                {
+                    ScenarioContext.Current["androidContext"] = AndroidContext;
+                }
+                else
+                {
+                    ScenarioContext.Current.Add("androidContext", AndroidContext);
+                }
+
+                Console.WriteLine("Appium Initialized for Scenario.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error Initializing Appium: {ex.Message}");
+                throw;
+            }
         }
+
         [AfterScenario]
         public void CleanUp()
         {
-            AppiumUtilities.CloseAppiumServer();
+            if (AppiumServiceContext != null)
+            {
+                Console.WriteLine("Stopping Appium Server...");
+                AppiumServiceContext.Dispose();
+                Console.WriteLine("Appium Server Stopped.");
+            }
         }
     }
 }
