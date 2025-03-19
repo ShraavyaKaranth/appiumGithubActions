1f0907746621d5b8996012b208253443e5a97cd6
ShraavyaKaranth <shravyabahha@gmail.com>
2025-03-19 15:21:48 +0530

    Update ClickPage.cs

--- Pages/ClickPage.cs
+++ Pages/ClickPage.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,61 +15,58 @@ namespace AppiumSpecFlowProject1.Pages
     public class ClickPage
     {
         private AndroidDriver _driver;
+
         public ClickPage(AndroidDriver androidDriver)
         {
             this._driver = androidDriver;
             PageFactory.InitElements(androidDriver, this);
-
         }
 
         [FindsBy(How = How.XPath, Using = "//android.widget.TextView[@content-desc=\"Views\"]")]
         private IWebElement Views { get; set; }
-        [FindsBy(How = How.XPath, Using = "//android.widget.TextView")]
-        private IWebElement elements { get; set; }
+
         [FindsBy(How = How.XPath, Using = "//android.widget.TextView[@content-desc=\"Radio Group\"]")]
         private IWebElement radio { get; set; }
+
         [FindsBy(How = How.XPath, Using = "//android.widget.RadioButton[@content-desc=\"Dinner\"]")]
         private IWebElement dinner { get; set; }
 
-
         public void ClickViews()
         {
-
+            Console.WriteLine("Clicking 'Views' Element...");
             Views.Click();
         }
+
         public void HandlingRadioCheckboxes()
         {
-            ActionBuilder actionBuilder = new ActionBuilder();
-            var touch = new OpenQA.Selenium.Appium.Interactions.PointerInputDevice(PointerKind.Touch, "finger");
-
-            //IWebElement View = _driver.FindElement(By.XPath("//android.widget.TextView[@content-desc=\"Views\"]"));
-            //View.Click();
+            Console.WriteLine("Handling Radio Checkboxes...");
 
             IList<AppiumElement> elements = _driver.FindElements(MobileBy.ClassName("android.widget.TextView"));
+            if (elements.Count < 14)
+            {
+                Console.WriteLine("Error: Not enough elements found.");
+                return;
+            }
+
             var origin = elements[13];
-            var loc1 = origin.Location;
             var target = elements[1];
-            var loc2 = target.Location;
-            actionBuilder.ClearSequences();
 
-            //move to element
+            var touch = new PointerInputDevice(PointerKind.Touch, "finger");
+            var actionBuilder = new ActionBuilder();
+
             actionBuilder.AddAction(touch.CreatePointerMove(origin, 0, 0, TimeSpan.FromMilliseconds(800)));
-
-            //touch to element
             actionBuilder.AddAction(touch.CreatePointerDown(PointerButton.TouchContact));
             actionBuilder.AddAction(touch.CreatePause(TimeSpan.FromMilliseconds(800)));
             actionBuilder.AddAction(touch.CreatePointerMove(target, 0, 0, TimeSpan.FromMilliseconds(800)));
             actionBuilder.AddAction(touch.CreatePointerUp(PointerButton.TouchContact));
 
-            var sequenceActions = actionBuilder.ToActionSequenceList();
-            _driver.PerformActions(sequenceActions);
+            _driver.PerformActions(actionBuilder.ToActionSequenceList());
 
-            IWebElement RadioGroup = _driver.FindElement(By.XPath("//android.widget.TextView[@content-desc=\"Radio Group\"]"));
-            RadioGroup.Click();
-            IWebElement DinnerRadio = _driver.FindElement(By.XPath("//android.widget.RadioButton[@content-desc=\"Dinner\"]"));
-            DinnerRadio.Click();
+            Console.WriteLine("Clicking 'Radio Group'...");
+            radio.Click();
 
-
+            Console.WriteLine("Clicking 'Dinner' Radio Button...");
+            dinner.Click();
         }
     }
 }
